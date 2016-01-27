279842a00348cf0b1d94fb4385bb5db0afff4af5
archie94 <arkaprava94@gmail.com>
2016-01-27 14:39:18 +0530

    Code modified

--- src/Practice/LinkedList.java
+++ src/Practice/LinkedList.java
@@ -15,7 +15,7 @@ public class LinkedList {
 		Scanner in = new Scanner(System.in);
 		int ch;
 		do{
-			System.out.println("1.insert 2.display 3.Delete by number 4.terminate");
+			System.out.println("1.insert 2.display 3.Delete by number 4.Delete by pos 5.Terminate");
 			ch = in.nextInt();
 			switch(ch){
 			case 1:head = insert(head,in.nextInt());
@@ -24,8 +24,10 @@ public class LinkedList {
 				break;
 			case 3:head = deleteByNum(head,in.nextInt());
 				break;
+			case 4:head = deleteByPos(head,in.nextInt());
+				break;
 			}
-		}while(ch!=4);
+		}while(ch!=5);
 	}
 	static Node insert(Node head,int num){
 		Node temp = new Node();
@@ -61,6 +63,31 @@ public class LinkedList {
 		Node obj1 = null;
 		for(;obj!=null;obj = obj.next){
 			if(obj.data == num){
+				break;
+			}
+			obj1 = obj;
+		}
+		if(obj == head){
+			head = head.next;
+		}
+		else if(obj == null){
+			System.out.println("Not found");
+		}
+		else{
+			obj1.next = obj.next;
+		}
+		return head;
+	}
+	static Node deleteByPos(Node head, int pos){
+		if(head == null){
+			System.out.println("List empty");
+			return head;
+		}
+		Node obj = head;
+		Node obj1 = null;
+		int c = 0;
+		for(;obj!=null;obj = obj.next){
+			if(++c == pos){
 				break;
 			}
 			obj1 = obj;
