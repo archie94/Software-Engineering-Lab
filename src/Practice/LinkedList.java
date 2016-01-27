3a1bd6de7c587c61095e8f76806015af3211b296
archie94 <arkaprava94@gmail.com>
2016-01-27 14:17:32 +0530

    Code modified

--- src/Practice/LinkedList.java
+++ src/Practice/LinkedList.java
@@ -15,15 +15,17 @@ public class LinkedList {
 		Scanner in = new Scanner(System.in);
 		int ch;
 		do{
-			System.out.println("1.insert 2.display 3.terminate");
+			System.out.println("1.insert 2.display 3.Delete by number 4.terminate");
 			ch = in.nextInt();
 			switch(ch){
 			case 1:head = insert(head,in.nextInt());
 				break;
 			case 2:display(head);
 				break;
+			case 3:head = deleteByNum(head,in.nextInt());
+				break;
 			}
-		}while(ch!=3);
+		}while(ch!=4);
 	}
 	static Node insert(Node head,int num){
 		Node temp = new Node();
@@ -49,5 +51,29 @@ public class LinkedList {
 			System.out.print(obj.data + " ");
 		}
 		System.out.println();
+	}
+	static Node deleteByNum(Node head,int num){
+		if(head == null){
+			System.out.println("List empty");
+			return head;
+		}
+		Node obj = head;
+		Node obj1 = null;
+		for(;obj!=null;obj = obj.next){
+			if(obj.data == num){
+				break;
+			}
+			obj1 = obj;
+		}
+		if(obj == head){
+			head = head.next;
+		}
+		else if(obj == null){
+			System.out.println("Not found");
+		}
+		else{
+			obj1.next = obj.next;
+		}
+		return head;
 	}
 }
