115737ffe315c1f5efdcb6ff0eebb390a1a7c721
archie94 <arkaprava94@gmail.com>
2016-02-10 14:05:37 +0530

    Codes documented

--- src/Class/BSearchUtil.java
+++ src/Class/BSearchUtil.java
@@ -1,24 +1,40 @@
+/*
+ * Filename : BinarySearch.java
+ */
 package Class;
 
 import java.util.*;
+/*
+ * The BinarySearchUtil class provides a main method for implementing Binary Search 
+ * in an array of integers
+ * @author : Arka Prava Basu
+ * @version 10 February 2016
+ */
 public class BSearchUtil extends BinarySearch{
+	/*
+	 * The main method takes a user defined array of integers and implements Binary Search 
+	 * @input : array_int[] : the array of integers
+	 * @input : array_size_int : the size of the array
+	 * @input : key : the key to be searched 
+	 */
 	public static void main(String args[]){
-		int[] arr;
-		int size;
-		int item;
-		int i;
+		int[] array_int;
+		int array_size_int;
+		int key;
+		/*
+		 * Scanner class object for user input 
+		 */
 		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter array size and then the integers in array");
-		size = in.nextInt();
-		arr = new int[size];
-		for(i=0;i<size;i++){
-			arr[i] = in.nextInt();
+		System.out.println("Enter array size and then the integers in array followed by the element to be searched");
+		array_size_int = input.nextInt();
+		array_int = new int[array_size_int];
+		for(int counter=0; counter<array_size_int; counter++){
+			array_int[counter] = input.nextInt();
 		}
-		System.out.println("Enter number to be searched");
-		item = in.nextInt();
+		key = input.nextInt();
 		
-		bSearch(arr,size,item);
+		binarySearch(array_int,array_size_int,key);
 	}
 }
--- src/Class/BinarySearch.java
+++ src/Class/BinarySearch.java
@@ -1,29 +1,64 @@
+/*
+ * Filename : BinarySearch.java
+ */
+
 package Class;
 
 import java.util.*;
+/*
+ * The BinarySearch class provides a static method binarySearch for 
+ * searching an integer in a sorted / unsorted array of integers
+ * @author : Arka Prava Basu
+ * @version 10 February 2016
+ */
 public class BinarySearch {
-	public static void bSearch(int[] arr,int size, int item){
-		int start;
-		int end;
-		int mid;
-		start = 0;
-		end = size-1;
-		Arrays.sort(arr);
-		while(start<=end){
-			mid = (start+end)/2;
-			if(arr[mid] == item){
-				System.out.println("Found at pos "+(mid+1));
+	/*
+	 * The binarySearch() method reads from the array of integers and then searches 
+	 * for the key in that array
+	 * @param : array_int[] : the array of integers
+	 * @param : array_size_int : the size of the array
+	 * @param : key_int : the key to be searched 
+	 * @output : displays the result of search with displayResult() method
+	 */
+	public static void binarySearch(int[] array_int,int array_size_int, int key_int){
+		int start_int = 0;
+		int end_int = array_size_int-1;
+		int middle_int;
+		/*
+		 * Sort the user given array
+		 */
+		Arrays.sort(array_int);
+		/*
+		 * The key is present in array_int[start_int .. end_int] or not present
+		 */
+		while(start_int <= end_int){
+			middle_int = (start_int+end_int)/2;
+			if(array_int[middle_int] == key_int){
+				displayResult(middle_int); //  display result with middle_int as resultCode
 				return;
 			}
-			else if(item > arr[mid]){
-				start = mid+1;
+			else if(key_int > array_int[middle_int]){
+				start_int = middle_int+1;
 			}
 			else{
-				end = mid-1;
+				end_int = middle_int-1;
 			}
 		}
-		if(start>end){
+		if(start_int > end_int){
+			displayResult(-1);// display result .. -1 as resultCode to signify search failure 
+		}
+	}
+	/*
+	 * The displayResult() displays the result of search
+	 * @param : resultCode_int : the result code which signifies the success of search
+	 * @input : none
+	 * @output : Displays the result of search
+	 */
+	public static void displayResult(int resultCode_int){
+		if(resultCode_int == -1){
 			System.out.println("Not found");
+		}else {
+			System.out.println("Found at pos "+(resultCode_int+1));
 		}
 	}
 }
